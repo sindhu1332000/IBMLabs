5fbfa5854b64f00b98c5839b8f3559b49d7f3947
sindhu1332000 <60882877+sindhu1332000@users.noreply.github.com>
2020-02-10 18:40:46 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,5 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+sum= float(a) + float(b)
+print("The sum is",format(sum))
---
+++ cal.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+sum = float(a) + float(b)
+sub = float(a) - float(b)
+mul = float(a) * float(b)
+div = float(a) / float(b)
+
+print("The values are",sum, sub, mul, div)
