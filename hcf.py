40ae1ffdc9add803ea15df14f30e1eb71b40b1db
sindhu1332000 <60882877+sindhu1332000@users.noreply.github.com>
2020-02-28 15:12:27 +0530

    Add files via upload

---
+++ hcf.py
@@ -0,0 +1,13 @@
+def compute_hcf(x,y):
+    #choose the smaller number
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller + 1):
+        if(x % i == 0) and (y % i == 0):
+            hcf = i
+    return hcf
+num1 = int(input("Enter a new number:"))
+num2 = int(input("Enter a new number:"))
+print("The hcf is", compute_hcf(num1, num2))
